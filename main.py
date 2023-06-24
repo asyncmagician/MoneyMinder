6d021ed10dc6b3f4e8143492c9748132e9af8ec3
PROJKT <giammarino.antony@gmail.com>
2023-06-24 10:58:09 +0200

    💄 Update UI

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from db.models import Base
 from datetime import datetime
 from utils.helpers import validate_input
 from utils.title import print_money_minder
+from colorama import Fore, Style
 import os
 
 load_dotenv()  
@@ -20,7 +21,8 @@ Base.metadata.create_all(db_engine)
 
 try:
     while True:
-        print("What do you want to do?")
+        print("\n" + "-" * 50)
+        print("\nWhat do you want to do?")
         print("")
         print("[1] Check my balance")
         print("[2] Add an expense")
@@ -31,6 +33,7 @@ try:
         print("[7] View expenses for a specific month")
         print("[8] Update balance")
         print("[0] Quit")
+        print("-" * 50 + "\n")        
 
 
         choice = input("Please, select a number:  ")
@@ -38,11 +41,22 @@ try:
         if choice == "1":
             most_recent_balance_update = get_most_recent_balance_update(db_engine)
             if most_recent_balance_update:
-                formatted_date = most_recent_balance_update.updated_at.strftime("%d/%m/%Y at %I:%M %p")
-                print(f"The most recent balance update was on {formatted_date} "
-                    f"and the balance was {most_recent_balance_update.account_balance}€")
+                formatted_date = most_recent_balance_update.updated_at.strftime("%d/%m/%Y at %I:%M%p")
+                balance = most_recent_balance_update.account_balance
+
+                color = Style.RESET_ALL
+
+                if balance < 0:
+                    color = Fore.RED
+                elif 0 <= balance < 500:
+                    color = Fore.YELLOW
+                elif balance > 500:
+                    color = Fore.GREEN
+
+                print(f"➪ The most recent balance update was on {formatted_date} "
+                    f"and the balance was {color}{balance}€{Style.RESET_ALL}")
             else:
-                print("No balance updates found for the current month.")
+                print("➪ No balance updates found for the current month.")
         elif choice == "2":
             transaction_data = {
                 "transaction_date": datetime.now(),
@@ -55,9 +69,9 @@ try:
 
             if validate_input(transaction_data):
                 create_transaction(db_engine, transaction_data)
-                print("The expense has been successfully added.")
+                print("➪ The expense has been successfully added.")
             else:
-                print("Invalid input. Please try again.")
+                print("➪ Invalid input. Please try again.")
             
         elif choice == "3":
             transaction_data = {
@@ -71,9 +85,9 @@ try:
 
             if validate_input(transaction_data):
                 create_transaction(db_engine, transaction_data)
-                print("The refund has been successfully added.")
+                print("➪ The refund has been successfully added.")
             else:
-                print("Invalid input. Please try again.")
+                print("➪ Invalid input. Please try again.")
         elif choice == "4":
             # update status logic
             pass
@@ -91,13 +105,13 @@ try:
                 "account_balance": float(input("Your balance : ")),
             }
             create_balance(db_engine, transaction_data)
-            print("The balance has been successfully updated.")
+            print("➪ The balance has been successfully updated.")
         elif choice == "0":
             db_engine.dispose()
             break
         else:
-            print("Invalid choices. Please try again.")
+            print("➪ Invalid choices. Please try again.")
 except KeyboardInterrupt:
-    print("\nProgram interrupted by user. Exiting...")
+    print("➪ \nProgram interrupted by user. Exiting...")
 finally:
     db_engine.dispose()
