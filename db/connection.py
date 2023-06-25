7c60b11c5b262d74e7f7c9c639374cde22f2fd6a
PROJKT <giammarino.antony@gmail.com>
2023-06-25 19:34:11 +0200

    💄 Update UI

--- db/connection.py
+++ db/connection.py
@@ -5,6 +5,7 @@ def create_db_engine(host, user, password, db):
     engine = None
     try:
         engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{db}")
+        print()
         print("✅ \033[32mConnection to MySQL database has been successful\033[0m")
         print("")
     except Error as e:
